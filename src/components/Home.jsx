57def7a0565b8f7c54841081cb9932da4fff2cf5
Vijayrajvijay <vr104555@gmail.com>
2023-10-10 15:38:22 +0530

    update media query

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,8 @@ import {Link} from 'react-router-dom'
 
 function Home() {
   return ( <>
-    <section id="hero" className="d-flex align-items-center">
-    <div className="container" data-aos="zoom-out" data-aos-delay="100">
+    <section id="hero" className="d-flex align-items-center col-md-">
+    <div className="container " id='homein' data-aos="zoom-out" data-aos-delay="100">
       <h1>Welcome to <span>Notes App</span></h1>
       <h2>This is simple application for taking a notes</h2>
       <Link to='/note'>
